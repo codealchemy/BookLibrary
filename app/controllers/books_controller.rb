f0909df88edca60d7ecd71c8004e73ede2879e1e
codealchemy <alchemitt@gmail.com>
2015-01-04 13:15:32 -0800

    Add book creation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_filter :find_book, only: [:show]
   before_action :authenticate_user!
 
   def index
@@ -10,11 +11,31 @@ class BooksController < ApplicationController
   end
 
   def create
-
+    @book = Book.new(book_params)
+      if @book.save
+        redirect_to books_path, notice: "Book saved"
+      else
+        redirect_to new_book_path, alert: "There's an error - please check the required fields"
+      end
   end
 
   def show
+  end
+
+  private
+
+  def find_book
     @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(
+        :title,
+        :isbn,
+        :author_first_name,
+        :author_last_name,
+        :description
+      )
   end
 
 end
