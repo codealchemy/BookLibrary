09fb92f94765fb3675815523ea3ab942682a1cdf
codealchemy <alchemitt@gmail.com>
2015-01-04 12:27:36 -0800

    Add user
    Both user and admin for separate permission sets - will scope actions
    to users (check out, return, etc.) and admins (create books, add
    admins, see overdue books)

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
+gem 'formtastic'
+gem 'devise'
 gem 'bootstrap-sass', '~> 3.3.1'
 gem 'autoprefixer-rails'
 gem 'amazon-ecs'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  root "books#index"
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }  
+
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
