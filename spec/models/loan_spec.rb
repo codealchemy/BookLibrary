6e5728c122f96d3c3d9b398663b35a1f7b50d738
codealchemy <alchemitt@gmail.com>
2015-01-07 20:40:32 -0800

    Simple validation spec for loans

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Loan, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#validations" do
+    let(:user) { User.create(email: 'alex@example.com', password: 'abc1das23456') }
+    let(:book) { Book.create(title: "A new start", isbn: "234-432-55-123") }
+
+    it "is invalid without a book" do
+      loan = Loan.new(user: user)
+      expect(loan.valid?).to eq(false)
+    end
+
+    it "is invalid without a user" do
+      loan = Loan.new(book: book)
+      expect(loan.valid?).to eq(false)
+    end
+  end
 end
