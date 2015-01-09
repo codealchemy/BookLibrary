3c7bf145c742d588354bb7623f0364ddcfb56e5c
codealchemy <alchemitt@gmail.com>
2015-01-08 22:26:05 -0800

    Tweaking views and whatnot

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+
+  def users_full_names
+    users = []
+    User.find_each { |user| users << user.name}
+    users
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] 
   resources :books
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
