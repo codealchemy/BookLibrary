57d89aa828a42d35f0b5a0e1a7bd98344667cffb
codealchemy <alchemitt@gmail.com>
2015-01-09 20:04:25 -0800

    Update auths for users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,34 @@
 class UsersController < ApplicationController
+  before_filter :authorize_admin
 
   def index
-    if current_user.admin?
       @users = User.all
-    else
-      @users = []
-    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to users_admin_index_path, notice: "User saved"
+      else
+        redirect_to new_users_admin_path, alert: "There's an error - please check the required fields"
+      end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation,
+        :admin
+      )
   end
 
 end
