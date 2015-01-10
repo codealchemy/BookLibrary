7e15ad5a559cfeb5c254cdce07fac9be452ac72c
codealchemy <alchemitt@gmail.com>
2015-01-10 10:47:41 -0800

    Helpers

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,9 +1,7 @@
 module BooksHelper
-
   def users_full_names
     users = []
-    User.find_each { |user| users << user.name}
+    User.find_each { |user| users << user.name }
     users
   end
-
 end
