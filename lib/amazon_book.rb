1d5c67b20e0a5b370d7df65b1a387e09b9f4d1df
Gavin DeSchutter <gavin.deschutter@mac.com>
2015-01-11 12:36:43 -0800

    show links/image with book

--- lib/amazon_book.rb
+++ lib/amazon_book.rb
@@ -5,46 +5,28 @@ class AmazonBook
   def initialize(isbn)
     @isbn = isbn
     @item = item_lookup
-    @main_page_link = retrieve_main_page_link
-    @image_link = retrieve_image_link
-    @reviews_link = retrieve_reviews_link
-    @marketplace_link = retrieve_marketplace_link
+    collect_links
   end
 
   def self.search_by_isbn(isbn)
     new(isbn)
   end
 
+  def collect_links
+    return unless isbn_and_item
+    self.main_page_link = retrieve_main_page_link
+    self.image_link = retrieve_image_link
+    self.reviews_link = retrieve_reviews_link
+    self.marketplace_link = retrieve_marketplace_link
+  end
+
   private
-  def retrieve_image_link
-    exit_if_no_isbn_or_item
-    image = image_lookup.items.first
-    return unless image
-    image.get_element('LargeImage/URL').to_s.gsub(/<URL>|<\/URL>/,'')
-  end
-
-  def retrieve_main_page_link
-    exit_if_no_isbn_or_item
-    item.get_hash['DetailPageURL']
-  end
-
-  def retrieve_reviews_link
-    exit_if_no_isbn_or_item
-    raw_link = item.get_array('ItemLink').select{|i| i =~ /reviews/i }.first
-    link = raw_link.match(/<URL>(.*?)<\/URL>/)[1] if raw_link
-  end
-
-  def retrieve_marketplace_link
-    exit_if_no_isbn_or_item
-    raw_link = item.get_array('ItemLink').select{|i| i =~ /all offers/i }.first
-    link = raw_link.match(/<URL>(.*?)<\/URL>/)[1] if raw_link
-  end
-
-  def exit_if_no_isbn_or_item
-    return unless isbn && item
+  def isbn_and_item
+    isbn && item
   end
 
   def item_lookup
+    return unless isbn
     AMZ.item_lookup(isbn,
       {
         id_type: 'ISBN',
@@ -57,5 +39,25 @@ class AmazonBook
 
   def image_lookup
     AMZ.item_search(isbn, response_group: 'Images')
+  end
+
+  def retrieve_image_link
+    image = image_lookup.items.first
+    return unless image
+    image.get_element('LargeImage/URL').to_s.gsub(/<URL>|<\/URL>/,'')
+  end
+
+  def retrieve_main_page_link
+    item.get_hash['DetailPageURL']
+  end
+
+  def retrieve_marketplace_link
+    raw_link = item.get_array('ItemLink').select{|i| i =~ /all offers/i }.first
+    link = raw_link.match(/<URL>(.*?)<\/URL>/)[1] if raw_link
+  end
+
+  def retrieve_reviews_link
+    raw_link = item.get_array('ItemLink').select{|i| i =~ /reviews/i }.first
+    link = raw_link.match(/<URL>(.*?)<\/URL>/)[1] if raw_link
   end
 end
