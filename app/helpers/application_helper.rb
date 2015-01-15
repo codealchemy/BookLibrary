d5b72b5a9009b0cd4bbaac35e873a7889a1c0421
codealchemy <alchemitt@gmail.com>
2015-01-14 20:25:06 -0800

    Add some basic styling to it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def sortable(column, title=nil)
+  def sortable(column, title = nil)
     title ||= column.titleize
-    direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
+    direction = column == params[:sort] && params[:direction] == 'asc' ? 'desc' : 'asc'
     link_to title, sort: column, direction: direction
   end
 
