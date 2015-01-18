accc4d838bc6c28a976403b11498cca713e8ec99
codealchemy <alchemitt@gmail.com>
2015-01-17 21:11:40 -0800

    Model tests

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,8 @@ class Location < ActiveRecord::Base
   has_many :users
   has_many :books
 
+  validates :name, presence: true
+
   def available_books
     books = []
     Book.find_each { |book| book.location == self && book.borrowed? == false ? books << book : next }
