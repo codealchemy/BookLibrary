b2632f761833460f3fb0bddae73c8a87b1e33bd3
codealchemy <alchemitt@gmail.com>
2015-01-20 20:42:45 -0800

    Locations controller

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,30 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe LocationsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+
   describe 'GET #index' do
-    it 'shows an list of locations for the user'
-    it 'renders the :index view'
+    before do
+      sign_in(user)
+    end
+
+    it 'renders the :index view' do
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
 
   describe 'GET #show' do
-    it 'shows the requested location to the user'
-    it 'renders the :show template'
+    let(:location) { create(:location) }
+
+    it 'renders the :show template' do
+      get :show, id: location.id
+      expect(response).to render_template(:show)
+    end
   end
 
   describe 'GET #new' do
-    it 'creates new location'
-    it 'renders the :new template'
+    it 'renders the :new template to admin' do
+      sign_in(admin)
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it 'redirects user to root path' do
+      sign_in(user)
+      get :new
+      expect(flash[:alert]).to eq('That\'s off limits!')
+      expect(response).to redirect_to(root_path)
+    end
   end
 
   describe 'POST #create' do
+    before do
+      sign_in(admin)
+    end
+
     context 'with valid attributes' do
-      it 'creates a new location'
-      it 'redirects to locations_path'
+      it 'creates a new location' do
+        expect {
+          post :create, location: FactoryGirl.attributes_for(:location)
+        }.to change { Location.count }.by(1)
+      end
+      it 'redirects to locations_path' do
+        post :create, location: FactoryGirl.attributes_for(:location)
+        expect(flash[:notice]).to eq('Location saved')
+        expect(response).to redirect_to(locations_path)
+      end
     end
 
     context 'with invalid attributes' do
-      it 'does not save the location in the database'
-      it 're-renders the :new template'
+      it 'does not save the location in the database' do
+        expect {
+          post :create, location: FactoryGirl.attributes_for(:location, name: nil)
+        }.not_to change { Location.count }
+      end
+      it 're-renders the :new template' do
+        post :create, location: FactoryGirl.attributes_for(:location, name: nil)
+        expect(flash[:alert]).to eq('There\'s an error - please check the required fields')
+        expect(response).to redirect_to(new_location_path)
+      end
     end
   end
 end
