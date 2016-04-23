d7f3be774e0d47a29f5765e79c0b398925670ecc
codealchemy <alchemitt@gmail.com>
2016-04-22 21:29:42 -0700

    Rewrite factories

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-  factory :book do |f|
-    f.title 'The Alchemist'
-    f.author_first_name 'Paulo'
-    f.author_last_name 'Coelho'
-    f.isbn '978-0061122415'
+  factory :book do
+    title             { 'The Alchemist' }
+    author_first_name { 'Paulo' }
+    author_last_name  { 'Coelho' }
+    isbn              { '978-0061122415' }
   end
 end
