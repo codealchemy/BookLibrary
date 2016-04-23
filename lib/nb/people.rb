247041804823c0eb6544854e2361729eb605ed8d
codealchemy <alchemitt@gmail.com>
2016-04-22 21:59:09 -0700

    Refactor Nb::People

--- lib/nb/people.rb
+++ lib/nb/people.rb
@@ -1,43 +1,30 @@
 module Nb
   class People < Service
 
-    def self.find_signup_image(user_email)
-      person_email_match(user_email)['person']['profile_image_url_ssl'] if person_email_match(user_email)['person']
+    def self.remove_tag(options)
+      user_id = find_nbid_in_nation(options[:email])
+      client.call(:people, :tag_removal, id: user_id, tag: options[:tag_to_remove])
     end
 
-    def self.find_self_in_nation
-      @token_owner_id = client.call(:people, :me)['person']['id']
-    end
-
-    def self.remove_tag(user_email, tag_name)
-      user_id = find_nbid_in_nation(user_email)
-      client.call(:people,
-                  :tag_removal,
-                  id: user_id,
-                  tag: tag_name)
-    end
-
-    def self.add_tag(user_email, tag_name)
-      user_id = find_nbid_in_nation(user_email)
-      client.call(:people,
-                  :tag_person,
-                  id: user_id,
-                  tagging:
-                  {
-                    tag: tag_name
-                  })
+    def self.add_tag(options)
+      user_id = find_nbid_in_nation(options[:email])
+      add_and_tag_person(options) unless user_id
+      client.call(:people, :tag_person, id: user_id, tagging: { tag: options[:tag_to_add] })
     end
 
     # Private class methods
 
-    def self.find_nbid_in_nation(user_email)
-      person_email_match(user_email)['person']['id'] if person_email_match(user_email)['person']
+    def self.add_and_tag_person(options)
+      client.call(:people, :add, person: { email: options[:email], tags: options[:tag_to_add] })
     end
 
-    def self.person_email_match(user_email)
-      @person_email_match ||= client.call(:people, :match, person: { email: user_email })
+    def self.find_nbid_in_nation(email)
+      match = client.call(:people, :match, email: email)
+      @matched_id = match['person']['id'] if match
+    rescue NationBuilder::ClientError
+      # Person doesn't exist in the nation
     end
 
-    private_class_method :find_nbid_in_nation, :match_on_email
+    private_class_method :find_nbid_in_nation, :add_and_tag_person
   end
 end
---
+++ spec/lib/nb/people_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe Nb::People do
+
+  let(:person_params) { { email: 'roger@example.com' } }
+
+  describe '#add_tag' do
+    it 'constructs a request to add a tag from person' do
+      params_with_tag = person_params.merge(tag_to_add: 'check this out')
+      allow(Nb::People).to receive(:find_nbid_in_nation).with(person_params[:email]).and_return(1)
+
+      expect_any_instance_of(NationBuilder::Client).to receive(:call)
+        .with(:people, :tag_person, id: 1, tagging: { tag: 'check this out' })
+      described_class.add_tag(params_with_tag)
+    end
+  end
+
+  describe '#remove_tag' do
+    it 'constructs a request to remove a tag from person' do
+      params_with_tag = person_params.merge(tag_to_remove: 'get rid of this')
+      allow(Nb::People).to receive(:find_nbid_in_nation).with(person_params[:email]).and_return(1)
+
+      expect_any_instance_of(NationBuilder::Client).to receive(:call)
+        .with(:people, :tag_removal, id: 1, tag: 'get rid of this')
+      described_class.remove_tag(params_with_tag)
+    end
+  end
+
+  # Private class methods
+
+  describe '#add_and_tag_person' do
+    it 'constructs a request to add and tag a person' do
+      params_with_tag = person_params.merge(tag_to_add: 'check this out')
+      expected_params = person_params.merge(tags: 'check this out')
+
+      expect_any_instance_of(NationBuilder::Client).to receive(:call)
+        .with(:people, :add, person: expected_params)
+      described_class.send(:add_and_tag_person, params_with_tag)
+    end
+
+    it 'gets a response in the form of a person hash' do
+      params_with_tag = person_params.merge(tag_to_add: 'check this out')
+
+      allow_any_instance_of(NationBuilder::Client).to receive(:call)
+        .and_return('person' => { 'id' => 1 })
+      described_class.send(:add_and_tag_person, params_with_tag)
+    end
+  end
+
+  describe '#find_nbid_in_nation' do
+    it 'constructs a request to match the person by email' do
+      allow_any_instance_of(NationBuilder::Client).to receive(:call)
+        .and_return('person' => { 'id' => 1 })
+
+      expect_any_instance_of(NationBuilder::Client).to receive(:call)
+        .with(:people, :match, email: 'test@example.com')
+
+      described_class.send(:find_nbid_in_nation, 'test@example.com')
+    end
+
+    it 'returns nil if no match is found' do
+      allow_any_instance_of(NationBuilder::Client).to receive(:call)
+        .and_raise(NationBuilder::ClientError)
+
+      allow_any_instance_of(NationBuilder::Client).to receive(:call)
+        .with(:people, :match, email: 'test@example.com')
+
+      response = described_class.send(:find_nbid_in_nation, 'test@example.com')
+      expect(response).to be_nil
+    end
+  end
+end
