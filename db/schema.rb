3e6591d6053ab57977da3e21bc7d6371e3fafac8
codealchemy <alchemitt@gmail.com>
2016-09-10 18:08:39 -0700

    Migration for adding authors and authorships join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118022601) do
+ActiveRecord::Schema.define(version: 20160911004639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: true do |t|
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "authorships", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "book_id",   null: false
+  end
+
+  add_index "authorships", ["author_id", "book_id"], name: "index_authorships_on_author_id_and_book_id", unique: true, using: :btree
+
+  create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
     t.string   "author_first_name"
@@ -32,7 +46,7 @@ ActiveRecord::Schema.define(version: 20150118022601) do
 
   add_index "books", ["location_id"], name: "index_books_on_location_id", using: :btree
 
-  create_table "loans", force: true do |t|
+  create_table "loans", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "book_id"
     t.datetime "checked_out_at"
@@ -44,7 +58,7 @@ ActiveRecord::Schema.define(version: 20150118022601) do
   add_index "loans", ["book_id"], name: "index_loans_on_book_id", using: :btree
   add_index "loans", ["user_id"], name: "index_loans_on_user_id", using: :btree
 
-  create_table "locations", force: true do |t|
+  create_table "locations", force: :cascade do |t|
     t.string   "name"
     t.string   "address1"
     t.string   "address2"
@@ -56,7 +70,7 @@ ActiveRecord::Schema.define(version: 20150118022601) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
