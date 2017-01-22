b17de433efaef420a82a82da73f5ee479b8bbcbb
codealchemy <alchemitt@gmail.com>
2017-01-21 18:58:28 -0800

    Drop now unused controllers/views/specs

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,4 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
-  resources :books, only: [:index, :show] do
-    post :check_out, on: :collection
-    post :check_in, on: :collection
-  end
-
-  resources :users_admin, only: [:show], controller: 'users'
-
-  resources :locations, only: [:index, :show]
 end
