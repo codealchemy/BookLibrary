ff7dcf153b2c807265a5fbe6f4b8497b1923c738
codealchemy <alchemitt@gmail.com>
2017-04-28 23:55:00 -0700

    Minor tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/app', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/app', as: :rails_admin
   root 'application#index'
 
   devise_for :users
-
 end
