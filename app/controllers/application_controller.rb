419c33259de3249b51e04b4b9b6fc1e2bc4bab88
codealchemy <alchemitt@gmail.com>
2017-04-29 14:40:39 -0700

    Handle unauthorized requests
    And remove index endpoint, which is no longer used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index; end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, alert: exception.message
+  end
 end
