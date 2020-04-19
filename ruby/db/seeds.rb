4515e0778b0c187b8b7e2eaed4c7ec9efbefd9eb
Alex Schmitt <alex@alchemist.codes>
2020-04-18 18:37:19 -0700

    Add some basic seed data
    Set up basic functionality for login and browsing books by location.

--- ruby/db/seeds.rb
+++ ruby/db/seeds.rb
@@ -1,4 +1,6 @@
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  admin = User.create!(first_name: 'Admin', email: 'admin@example.test', password: 'admin123', admin: true)
+  location = admin.create_location!(name: 'Home')
+  author = Author.create!(first_name: 'Paulo', last_name: 'Coelho')
+  location.books.create!(authors: [author], title: 'The Alchemist', isbn: '9780061122415')
+end
