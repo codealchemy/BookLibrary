1a9edd7e48c357a6c5d7c88f26a57cc7a5909777
Alex Schmitt <alex@alchemist.codes>
2020-05-03 19:53:33 -0700

    Fix seeds / base layout
    Missing favicon is throwing an error - we can add it back later.
    
    Seeds had pre-existing validation issues when running on a fresh db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 ActiveRecord::Base.transaction do
-  admin = User.create!(first_name: 'Admin', email: 'admin@example.test', password: 'admin123', admin: true)
-  location = admin.create_location!(name: 'Home')
+  location = Location.create!(name: 'Home')
+  admin = User.create!(first_name: 'Admin', email: 'admin@example.test', password: 'admin123', admin: true, location: location)
   author = Author.create!(first_name: 'Paulo', last_name: 'Coelho')
-  location.books.create!(authors: [author], title: 'The Alchemist', isbn: '9780061122415')
+  location.books.create!(authors: [author], title: 'The Alchemist', isbn: '9780061122415', owner: admin)
 end
